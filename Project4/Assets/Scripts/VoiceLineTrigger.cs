b3aed3595b422a264ec7bdb0092b3c1f8e89da82
ajl64 <ajawadiwar2@gmail.com>
2024-02-15 09:39:58 -0500

    Added in the voice lines
    just walk up near certain objects and it'll play a voice line.

---
+++ Project4/Assets/Scripts/VoiceLineTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLineTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioClip voiceLine;
+    [SerializeField] private AudioSource playerSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerSource.Stop();
+        playerSource.clip = voiceLine;
+        playerSource.Play();
+        Destroy(gameObject);
+    }
+}
